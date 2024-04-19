cc3fb2ba2c8a081f2b94630fab8a4d5113993f99
HollosJ <hollosj@gmail.com>
2024-04-19 22:50:40 +0100

    Replace copy on homepage with a hero

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,37 +1,34 @@
-import { useState } from 'react';
-import createTrip from '../utils/supabase/createTrip';
+import HeroImage from '@/assets/hero-image.svg';
 
 const Home = () => {
   return (
-    <div className="container my-8 md:my-16">
-      <h1 className="text-4xl md:text-7xl">Home</h1>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque rerum
-      nemo possimus! Rerum tenetur ut, ex blanditiis consequatur est minus nisi
-      odit sapiente, eaque accusamus error, tempora totam dolorum. Praesentium,
-      ab! Iste tempora quidem maiores? Blanditiis minus praesentium, quo
-      laboriosam commodi culpa facilis consequatur vel quas impedit rerum nemo
-      similique ipsa nihil. Esse non dolorum expedita, nobis itaque accusantium
-      eveniet minima asperiores pariatur aspernatur ipsam? Eaque nostrum quia
-      praesentium cupiditate alias blanditiis consequatur, quo facere, voluptate
-      voluptatibus minima saepe tempora neque ab quaerat molestiae! Minus cum
-      incidunt similique quisquam autem porro iure nemo nihil adipisci totam
-      cupiditate aliquid, consequatur assumenda consectetur aperiam, fugiat
-      voluptas unde, eligendi tenetur. Dolor et, consectetur cumque dignissimos
-      laborum excepturi ratione, iste, qui facere perspiciatis eligendi minima
-      inventore vero impedit repellat modi. Tenetur sequi voluptas iste,
-      possimus inventore labore quos ratione amet. Sint odio, beatae earum culpa
-      architecto nesciunt eligendi error labore eveniet distinctio quis
-      inventore esse, itaque explicabo vitae sed exercitationem praesentium,
-      corrupti voluptatibus iste. Cupiditate modi eveniet eius, quos facilis
-      voluptatem laborum. A quae corrupti debitis hic dicta aspernatur provident
-      expedita, veritatis est accusamus, modi molestias unde voluptas molestiae
-      at, harum rerum perferendis? Nisi quidem laboriosam consequatur eius error
-      obcaecati aperiam inventore reiciendis, earum cum corrupti sint natus amet
-      nemo. Non nemo odit voluptatibus laboriosam! Velit est aut, eaque
-      consectetur sapiente perferendis accusamus cum nesciunt voluptates ipsam
-      dolores? Facere eveniet autem laboriosam ullam cumque, dicta modi
-      consequuntur totam excepturi nesciunt laudantium blanditiis nobis suscipit
-      illo voluptates veniam et aut officiis voluptatem adipisci iure. Commodi!
+    <div className="container grid items-center gap-8 py-8 md:grid-cols-2 md:max-w-screen-lg">
+      <div className="grid gap-8 justify-items-start">
+        <h1 className="text-3xl font-bold lg:text-5xl">
+          Your{' '}
+          <span className="text-transparent gradient--green bg-clip-text">
+            Ultimate
+          </span>{' '}
+          Travel Planning Companion!
+        </h1>
+
+        {/* CTA */}
+        <div className="grid">
+          <button className="button button--primary">Get Started</button>
+
+          <p className="mt-2 text-gray-600">
+            Already have an account?{' '}
+            <a href="/login" className="font-semibold text-green-600">
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <img
+        src={HeroImage}
+        alt="Illustration of a lady with a suitcase next to a tree"
+      />
     </div>
   );
 };
