0a991d22d82593ebed8db5a3f896d6d04eec43c9
HollosJ <hollosj@gmail.com>
2024-05-03 20:16:49 +0100

    Add a close button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,6 +12,26 @@ const Modal = ({ trigger, setTrigger, children }) => {
 
           <div className="container flex items-center justify-center h-full">
             <div className="relative z-10 w-full max-w-screen-sm p-4 bg-white rounded-lg shadow-lg">
+              <button
+                className="absolute top-4 right-4"
+                onClick={() => setTrigger(false)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+
               {children}
             </div>
           </div>
