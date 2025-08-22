32af1e73eac13e3aeffd8ab70f3c9257d0af91ac
HollosJ <hollosj99@gmail.com>
2025-08-22 15:57:12 +0100

    Move server to root

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes/index.js";
-import dotenv from "dotenv";
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import routes from './src/routes/index.js';
+
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
