ef1f1d0fd28f9b873c12b70de64c9c86f05f8c40
HollosJ <hollosj99@gmail.com>
2025-08-22 15:57:43 +0100

    styling tweaks

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,14 +1,14 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import LoginForm from "../components/LoginForm";
+import { createFileRoute, Link } from '@tanstack/react-router';
+import LoginForm from '../components/LoginForm';
 
-export const Route = createFileRoute("/login")({
+export const Route = createFileRoute('/login')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <div className="container my-8 md:my-16">
-      <h1 className="font-bold text-3xl">Login</h1>
+    <div className="container my-8 md:my-16 md:max-w-screen-md">
+      <h1 className="font-bold text-3xl mb-8">Login</h1>
 
       <LoginForm />
 
--- frontend/src/routes/register.tsx
+++ frontend/src/routes/register.tsx
@@ -1,14 +1,14 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { RegisterForm } from "../components/RegisterForm";
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { RegisterForm } from '../components/RegisterForm';
 
-export const Route = createFileRoute("/register")({
+export const Route = createFileRoute('/register')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <div className="container my-8 md:my-16">
-      <h1 className="font-bold text-3xl">Register</h1>
+    <div className="container my-8 md:my-16 md:max-w-screen-md">
+      <h1 className="font-bold text-3xl mb-8">Register</h1>
 
       <RegisterForm />
 
