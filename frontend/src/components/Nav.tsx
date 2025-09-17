eccff707a4ee92db4adb8beb548c8e9c00317036
HollosJ <hollosj99@gmail.com>
2025-09-17 13:41:02 +0100

    ✨Redirect user to login page when logging out

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,30 +1,38 @@
 import { useAuth } from '../context/AuthProvider';
-import { Link } from '@tanstack/react-router';
+import { Link, useNavigate } from '@tanstack/react-router';
 
 export default function Nav() {
+  const navigate = useNavigate();
   const { user, logout } = useAuth();
 
   const isLoggedIn = !!user;
 
   return (
-    <nav className='border-b px-2 flex items-center justify-between gap-2 bg-white text-sm'>
-      <div className='flex gap-2'>
-        <Link to='/' className='[&.active]:font-bold'>
+    <nav className="flex items-center justify-between gap-2 border-b bg-white px-2 text-sm">
+      <div className="flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>
 
         {isLoggedIn && (
-          <Link to='/trips' className='[&.active]:font-bold'>
+          <Link to="/trips" className="[&.active]:font-bold">
             Trips
           </Link>
         )}
       </div>
 
-      <div className='flex gap-2'>
+      <div className="flex gap-2">
         {isLoggedIn ? (
-          <button onClick={logout}>Logout</button>
+          <button
+            onClick={() => {
+              navigate({ to: '/login' });
+              logout();
+            }}
+          >
+            Logout
+          </button>
         ) : (
-          <Link to='/login'>Login</Link>
+          <Link to="/login">Login</Link>
         )}
       </div>
     </nav>
