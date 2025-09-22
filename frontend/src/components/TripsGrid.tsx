fe653a491b0c3035c91e026d4d412028ae57d140
HollosJ <hollosj99@gmail.com>
2025-09-22 21:03:48 +0100

    feat: break trips into past and upcoming

--- frontend/src/components/TripsGrid.tsx
+++ frontend/src/components/TripsGrid.tsx
@@ -7,7 +7,7 @@ function TripsGridItem({ trip }: { trip: Trip }) {
       to={`/trips/$tripId`}
       params={{ tripId: String(trip.id) }}
       key={trip.id}
-      className="rounded bg-white p-4 shadow transition-opacity hover:opacity-50"
+      className="rounded bg-white p-4 shadow transition-opacity hover:opacity-75"
     >
       {trip.name}
     </Link>
@@ -20,15 +20,49 @@ interface TripsGridProps {
 }
 
 export default function TripsGrid({ trips, className }: TripsGridProps) {
+  const list = trips.reduce(
+    (acc: { upcoming: Trip[]; past: Trip[] }, item: Trip) => {
+      if (new Date(item.startDate) < new Date()) {
+        acc.past.push(item);
+      } else {
+        acc.upcoming.push(item);
+      }
+
+      return acc;
+    },
+    {
+      upcoming: [],
+      past: [],
+    },
+  );
+
   return (
     <div className={` ${className || ''}`}>
       {trips.length === 0 && <div>You have no trips yet!</div>}
 
-      <div className="grid gap-4 sm:grid-cols-3">
-        {trips.map((trip) => (
-          <TripsGridItem trip={trip} key={trip.id} />
-        ))}
-      </div>
+      {list.upcoming.length > 0 && (
+        <div className="grid gap-4">
+          <h2 className="text-lg">Upcoming trips</h2>
+
+          <div className="grid gap-8 md:grid-cols-3">
+            {list.upcoming.map((trip) => (
+              <TripsGridItem trip={trip} key={trip.id} />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {list.past.length > 0 && (
+        <div className="mt-4 grid gap-4">
+          <h2 className="text-lg">Past trips</h2>
+
+          <div className="grid gap-8 md:grid-cols-3">
+            {list.past.map((trip) => (
+              <TripsGridItem trip={trip} key={trip.id} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
