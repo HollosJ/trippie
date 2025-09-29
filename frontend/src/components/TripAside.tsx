9031016819f4ce28df4e3058dadc7a22a9ab71e8
HollosJ <hollosj99@gmail.com>
2025-09-29 16:10:02 +0100

    feat: Add desktop aside

--- frontend/src/components/TripAside.tsx
+++ frontend/src/components/TripAside.tsx
@@ -1,6 +1,9 @@
 import { Link } from '@tanstack/react-router';
+import { AnimatePresence, motion } from 'framer-motion';
 import {
+  ArrowLeft,
   CalendarIcon,
+  ChevronRight,
   CornerDownLeft,
   MoreVertical,
   Trash,
@@ -8,7 +11,6 @@ import {
 import { useEffect, useState } from 'react';
 import { useModal } from '../context/ModalProvider';
 import type { Trip } from '../types';
-import { AnimatePresence, motion } from 'framer-motion';
 
 interface TripAsideProps {
   trip: Trip;
@@ -28,6 +30,7 @@ export default function TripAside({ trip, handleDelete }: TripAsideProps) {
       return false;
     }
   });
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
 
   // Remember user preferencee
   useEffect(() => {
@@ -68,14 +71,13 @@ export default function TripAside({ trip, handleDelete }: TripAsideProps) {
   return (
     <>
       {/* Mobile */}
-      <div className="bg-primary fixed bottom-0 w-full p-4 text-white md:hidden">
+      <div className="bg-primary fixed bottom-0 w-full py-4 text-white md:hidden">
         <div className="container flex items-center justify-between gap-4">
           <Link
             to="/trips"
-            className="flex flex-col items-center gap-2 text-xs"
+            className="flex flex-col items-center gap-2 rounded bg-emerald-700 p-4 text-xs"
           >
             <CornerDownLeft className="size-4" />
-            <span>Return</span>
           </Link>
 
           <div className="flex flex-1 flex-col items-center">
@@ -87,29 +89,28 @@ export default function TripAside({ trip, handleDelete }: TripAsideProps) {
             </span>
           </div>
 
-          <div className="flex flex-col items-center gap-2 text-xs">
+          <div className="flex flex-col items-center gap-2 rounded bg-emerald-700 p-4 text-xs">
             <MoreVertical
               className="size-4"
-              onClick={() => setIsCollapsed(!isCollapsed)}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             />
-            <span>More</span>
           </div>
 
           {/* Context menu */}
           <AnimatePresence>
-            {!isCollapsed && (
+            {isMobileMenuOpen && (
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 20 }}
                 transition={{ duration: 0.2, ease: 'easeInOut' }}
-                className="absolute right-0 bottom-full w-full bg-emerald-800 p-4"
+                className="absolute right-0 bottom-full w-full bg-emerald-800"
               >
-                <div className="container flex items-center justify-between gap-4">
+                <div className="container flex items-center justify-between gap-4 py-4">
                   <button
                     type="button"
                     onClick={deleteTripModal}
-                    className="flex flex-col items-center gap-2 text-xs"
+                    className="text-danger flex flex-col items-center text-xs"
                   >
                     <Trash className="size-4" />
                     Delete Trip
@@ -122,6 +123,53 @@ export default function TripAside({ trip, handleDelete }: TripAsideProps) {
       </div>
 
       {/* Desktop */}
+      <aside
+        className={`relative hidden shrink-0 flex-col bg-gray-900 text-white transition-all md:flex ${isCollapsed ? 'w-16' : 'w-72'}`}
+      >
+        {/* Toggle */}
+        <button
+          className="absolute top-4 -right-3 z-10 flex size-6 items-center justify-center rounded-full bg-gray-800"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          <ChevronRight
+            className={`${isCollapsed ? '' : 'rotate-180'} size-4 transition-transform`}
+          />
+        </button>
+
+        {/* Content */}
+        <div className={`mt-8 flex flex-1 flex-col p-2 transition-all`}>
+          <Link
+            className="flex items-center justify-center gap-2 rounded bg-gray-800 p-2 hover:brightness-125"
+            to="/trips"
+          >
+            <ArrowLeft className="size-4" /> {!isCollapsed && 'Back to trips'}
+          </Link>
+
+          <button
+            className="btn btn--danger mt-2 gap-2"
+            onClick={deleteTripModal}
+          >
+            <Trash className="size-4" />
+
+            {!isCollapsed && 'Delete Trip'}
+          </button>
+
+          <h1
+            className={`mt-8 text-sm leading-0 font-normal ${isCollapsed ? '[writing-mode:vertical-lr]' : ''}`}
+          >
+            {!isCollapsed && (
+              <>
+                Your trip to <br />
+              </>
+            )}
+            <span
+              className={`bg-gradient-to-tr from-emerald-500 to-emerald-600 bg-clip-text font-bold hyphens-auto text-transparent ${isCollapsed ? 'text-4xl' : 'text-3xl'}`}
+            >
+              {trip.name}
+            </span>
+          </h1>
+        </div>
+      </aside>
     </>
   );
 }
