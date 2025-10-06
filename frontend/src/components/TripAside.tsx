1f79648f4497756cde620256abbd7703d56eead7
HollosJ <hollosj99@gmail.com>
2025-10-06 19:29:46 +0100

    feat:  some styling tweaks

--- frontend/src/components/TripAside.tsx
+++ frontend/src/components/TripAside.tsx
@@ -82,7 +82,10 @@ export default function TripAside({ trip, handleDelete }: TripAsideProps) {
         <h1
           className={`mt-4 ${isCollapsed ? '[writing-mode:vertical-lr]' : ''}`}
         >
-          <span className="text-2xl text-emerald-400">{trip.name}</span>
+          <span className="text-2xl text-emerald-400">
+            {trip.emoji ?? ''}
+            {trip.name}
+          </span>
         </h1>
 
         {!isCollapsed && daysUntilTrip > 0 && (
